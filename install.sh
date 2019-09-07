7c101eec92bdb9b1c7c85d1789bd288607682e2c
shrynshJn <shreyanshmacro@gmail.com>
2019-09-07 10:51:07 +0530

    commit

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install git
 sudo apt install python3-pip
-sudo apt install ns2
-sudo apt install nam
+#sudo apt install ns2
+#sudo apt install nam
 sudo apt install r-base
-sudo apt install iverilog
-sudo apt install gtkware
+#sudo apt install iverilog
+#sudo apt install gtkware
 sudo snap install vlc
 sudo snap install spotify
 sudo apt install curl
+sudo apt install nodejs
+sudo apt install npm
+sudo snap install android-studio --classic
+sudo snap install vlc
+sudo snap install sublime-text --classic
 sudo apt autoremove
